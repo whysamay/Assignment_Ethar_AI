217334ee16029f1886e0f2597f5fa8af4f375786
whysamay <samaypawar2200@gmail.com>
2026-04-30 20:51:57 +0530

    models classes created

--- models.py
+++ models.py
@@ -1,1 +1,32 @@
 from database import Base
+import datetime, email
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    phone_number = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String)
+
+class Project(Base):
+    __tablename__ = "projects"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    status = Column(String, default="pending") 
+    due_date = Column(DateTime, nullable=True)
+    
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    assignee_id = Column(Integer, ForeignKey("users.id"), nullable=True)
