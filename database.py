4d80dce6a02ec71487f9ac33fb4834060fc39980
whysamay <samaypawar2200@gmail.com>
2026-04-30 20:48:41 +0530

    database engine created

---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./todo.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# we are connecting sql db to engine and checksamethread is used to safely allow multiple
+#users to acces the same db at the same time
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#whenever session local is called, we will get a session object
+#this session object is used to perform crud operations on the db
+#autocommit false means commit when db.commit 
+
+Base = declarative_base()
+#parent class for all the models
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#used to create new sessions
